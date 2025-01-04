4859185371e536db846a46c503bfb824179e85ae
dan feithlicher <dan.f@weski.com>
2025-01-04 12:16:59 +0200

    little fixes

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,39 +1,22 @@
 import { Page, Layout } from "@shopify/polaris";
 import Sidebar from "../components/Sidebar/Sidebar.jsx";
-import Welcome from "../components/Welcome/Welcome.jsx";
-import SetupWizard from "../components/SetupWizard/SetupWizard.jsx";
 import "./index.css";
 import { useState } from "react";
-
-const RenderStepsTypes = {
-  Welcome: "Welcome",
-  SetupWizard: "SetupWizard",
-  Insights: "Insights",
-};
+import { RenderStepsTypes, stepsDictionary } from "./step-routs.jsx";
 
 export default function HomePage() {
   const [currentStep, setCurrentStep] = useState(RenderStepsTypes.Welcome);
-
-  const renderStep = () => {
-    switch (currentStep) {
-      case RenderStepsTypes.Welcome:
-        return <Welcome />;
-      case RenderStepsTypes.SetupWizard:
-        return <SetupWizard />;
-      case RenderStepsTypes.Insights:
-        return <div>Insights Component Placeholder</div>;
-      default:
-        return <Welcome />;
-    }
-  };
 
   return (
     <Page fullWidth={true}>
       <Layout>
         <Layout.Section>
           <div className="app">
-            <Sidebar setCurrentStep={setCurrentStep} currentStep={currentStep} />
-            <div className="content">{renderStep()}</div>
+            <Sidebar
+              setCurrentStep={setCurrentStep}
+              currentStep={currentStep}
+            />
+            <div className="content">{stepsDictionary[currentStep]}</div>
           </div>
         </Layout.Section>
       </Layout>
