9724ba1dc92e39f75c734a4ebd1056dfd48ab6b4
dan feithlicher <dan.f@weski.com>
2025-01-06 19:47:36 +0200

    new setup data state

--- web/frontend/components/SetupWizard/SetupWizard.jsx
+++ web/frontend/components/SetupWizard/SetupWizard.jsx
@@ -8,13 +8,27 @@ import QuestionsAndAnswers from "../QuestionsAndAnswers/QuestionsAndAnswers.jsx"
 import AIChatBotPreview from "../AIChatBotPreview/AIChatBotPreview.jsx";
 
 const SetupWizard = () => {
+  const [setupData, setSetupData] = useState({
+    initial_message: "hello!",
+    initial_suggestions: ["suggestions1", "suggestions2", "suggestions3"],
+    delivery_description: "description",
+    shop_description: "shop description",
+    general_information: "general information",
+    returns_policy: "returns_policy",
+    primary_color: "#FAFAFA",
+    FAQs: [
+      { question: "What is the color of the shoe?", answer: "500$" },
+      { question: "What is the color of the hat?", answer: "300$" },
+    ],
+  });
+
   const [data, setData] = useState({
-    chatBotConfig:{},
-    returnPolicy:{},
-    deliverPolicy:{},
-    initialSuggestions:{},
-    questionsAndAnswers:{}
-  })
+    chatBotConfig: {},
+    returnPolicy: {},
+    deliverPolicy: {},
+    initialSuggestions: {},
+    questionsAndAnswers: {},
+  });
 
   const updateData = useCallback((key, newData) => {
     setData((prevState) => ({
@@ -22,38 +36,31 @@ const SetupWizard = () => {
       [key]: newData,
     }));
   }, []);
-  
+
   const SaveChanges = () => {
     console.log("Saving data: ", data);
-  }
+  };
 
   return (
     <div className="setup-wizard">
       <h1 className="title">Setup Wizard</h1>
       <div className="setup-wizard__container">
         <section className="setup-wizard__container-column">
-        <ChatbotConfiguration 
-          updateData={updateData} 
-        />
+          <ChatbotConfiguration updateData={updateData} />
         </section>
         <section className="setup-wizard__container-column">
-          <ReturnPolicy 
-            updateData={updateData}
-          />
-          <DeliverPolicy 
-            updateData={updateData} 
-          />
-          <InitialSuggestions 
-            updateData={updateData}
-          />
+          <ReturnPolicy updateData={updateData} />
+          <DeliverPolicy updateData={updateData} />
+          <InitialSuggestions updateData={updateData} />
         </section>
         <section className="setup-wizard__container-column">
-          <QuestionsAndAnswers 
-            updateData={updateData}
-          />
+          <QuestionsAndAnswers updateData={updateData} />
           <AIChatBotPreview />
           <div className="setup-wizard__submit-container">
-            <button className="setup-wizard__submit-button" onClick={SaveChanges}>
+            <button
+              className="setup-wizard__submit-button"
+              onClick={SaveChanges}
+            >
               Save Changes
             </button>
           </div>
